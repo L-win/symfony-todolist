f5f900af0c4b85cf5b78a7578f8bae109937a686
Elvin <l-win@inbox.ru>
2018-06-06 00:53:26 +0300

    bootstrap layout added

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -9,26 +9,34 @@ use Symfony\Component\HttpFoundation\Request;
 class TodoController extends Controller
 {
     /**
-     * @Route("/todos", name="todo_list")
+     * @Route("/todo", name="todo_list")
      */
-    public function listAction(Request $request)
+    public function listAction()
     {
         return $this->render('todo/index.html.twig');
     }
 	
 	/**
-     * @Route("/todos/create", name="todo_create")
+     * @Route("/todo/create", name="todo_create")
      */
     public function createAction(Request $request)
     {
         return $this->render('todo/create.html.twig');
     }
-	s
+
 	/**
-     * @Route("/todos/edit{id}", name="todo_edit")
+     * @Route("/todo/edit/{id}", name="todo_edit")
      */	
-    public function editAction(Request $request)
+    public function editAction($id, Request $request)
     {
         return $this->render('todo/edit.html.twig');
+    }
+
+	/**
+     * @Route("/todo/details/{id}", name="todo_details")
+     */	
+    public function detailsAction($id)
+    {
+        return $this->render('todo/details.html.twig');
     }
 }
