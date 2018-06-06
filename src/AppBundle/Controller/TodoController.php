18674d810cadbff7b2e8cbd91fe4dea85f51f04d
Elvin <l-win@inbox.ru>
2018-06-06 15:57:21 +0300

    create todo form added

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -2,9 +2,16 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Todo;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class TodoController extends Controller
 {
@@ -13,7 +20,8 @@ class TodoController extends Controller
      */
     public function listAction()
     {
-        return $this->render('todo/index.html.twig');
+		$todos = $this->getDoctrine()->getRepository('AppBundle:Todo')->findAll();
+        return $this->render('todo/index.html.twig', array('todos'=>$todos));
     }
 	
 	/**
@@ -21,7 +29,20 @@ class TodoController extends Controller
      */
     public function createAction(Request $request)
     {
-        return $this->render('todo/create.html.twig');
+		$todo = new Todo;
+		$form = $this->createFormBuilder($todo)
+			->add('name', TextType::class, array('attr'=>array('class'=>'form-control', 'style'=>'margin-bottom:15px;')))
+			->add('category', TextType::class, array('attr'=>array('class'=>'form-control', 'style'=>'margin-bottom:15px;')))
+			->add('description', TextareaType::class, array('attr'=>array('class'=>'form-control', 'style'=>'margin-bottom:15px;')))
+			->add('priority', ChoiceType::class, array('choices' => array('Low'=>'Low', 'Normal'=>'Normal', 'Normal'=>'Normal') ,'attr'=>array('class'=>'form-control', 'style'=>'margin-bottom:15px;')))
+			->add('due_date', DateTimeType::class, array('attr'=>array('class'=>'form-control', 'style'=>'margin-bottom:15px;')))
+			->add('save', SubmitType::class, array('attr'=>array('class'=>'btn btn-primary', 'style'=>'margin-bottom:15px;')))
+			->getForm();
+		$form->handleRequest($request);
+		if( $form->isSubmitted() AND $form->isValid() ){
+			echo '11111';
+		}
+		return $this->render('todo/create.html.twig', array('form'=>$form->createView()));
     }
 
 	/**
