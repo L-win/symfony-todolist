83a892ee02db5d4a630f8da8b3ee44d0e6f3b53a
Elvin <l-win@inbox.ru>
2018-06-06 21:16:03 +0300

    todo details page

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -15,21 +15,22 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class TodoController extends Controller
 {
-    /**
-     * @Route("/todo", name="todo_list")
-     */
-    public function listAction()
-    {
+	/**
+	 * @Route("/todo", name="todo_list")
+	 */
+	public function listAction()
+	{
 		$todos = $this->getDoctrine()->getRepository('AppBundle:Todo')->findAll();
-        return $this->render('todo/index.html.twig', array('todos'=>$todos));
-    }
+		return $this->render('todo/index.html.twig', array('todos'=>$todos));
+	}
 	
 	/**
-     * @Route("/todo/create", name="todo_create")
-     */
-    public function createAction(Request $request)
-    {
+	 * @Route("/todo/create", name="todo_create")
+	 */
+	public function createAction(Request $request)
+	{
 		$todo = new Todo;
+
 		$form = $this->createFormBuilder($todo)
 			->add('name', TextType::class, array('attr'=>array('class'=>'form-control', 'style'=>'margin-bottom:15px;')))
 			->add('category', TextType::class, array('attr'=>array('class'=>'form-control', 'style'=>'margin-bottom:15px;')))
@@ -39,25 +40,48 @@ class TodoController extends Controller
 			->add('save', SubmitType::class, array('attr'=>array('class'=>'btn btn-primary', 'style'=>'margin-bottom:15px;')))
 			->getForm();
 		$form->handleRequest($request);
+
 		if( $form->isSubmitted() AND $form->isValid() ){
-			echo '11111';
+			$name = $form['name']->getData();
+			$category = $form['category']->getData();
+			$description = $form['description']->getData();
+			$priority = $form['priority']->getData();
+			$due_date = $form['due_date']->getData();
+			
+			$cur_date = new\DateTime('now');
+			$todo->setName($name);
+			$todo->setCategory($category);
+			$todo->setDescription($description);
+			$todo->setPriority($priority);
+			$todo->setDueDate($due_date);
+			$todo->setCreateDate($cur_date);
+			
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($todo);
+			$em->flush();
+			
+			$this->addFlash('notice','Todo Added');
+			
+			return $this->redirectToRoute('todo_list');
 		}
+
 		return $this->render('todo/create.html.twig', array('form'=>$form->createView()));
-    }
+	}
 
 	/**
-     * @Route("/todo/edit/{id}", name="todo_edit")
-     */	
-    public function editAction($id, Request $request)
-    {
-        return $this->render('todo/edit.html.twig');
-    }
+	 * @Route("/todo/edit/{id}", name="todo_edit")
+	 */	
+	public function editAction($id, Request $request)
+	{
+		return $this->render('todo/edit.html.twig');
+	}
 
 	/**
-     * @Route("/todo/details/{id}", name="todo_details")
-     */	
-    public function detailsAction($id)
-    {
-        return $this->render('todo/details.html.twig');
-    }
+	 * @Route("/todo/details/{id}", name="todo_details")
+	 */	
+	public function detailsAction($id)
+	{
+		$todo = $this->getDoctrine()->getRepository('AppBundle:Todo')->find($id);
+		return $this->render('todo/details.html.twig', array('todo'=>$todo));
+	}
 }
