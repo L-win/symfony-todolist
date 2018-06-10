bbf0fe4d3e0a3f4a4b767221f4030b200248ebf4
Elvin <l-win@inbox.ru>
2018-06-10 08:44:11 +0300

    delete todo, bootstrap scripts, flash messages

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -84,4 +84,20 @@ class TodoController extends Controller
 		$todo = $this->getDoctrine()->getRepository('AppBundle:Todo')->find($id);
 		return $this->render('todo/details.html.twig', array('todo'=>$todo));
 	}
+
+	/**
+	 * @Route("/todo/delete/{id}", name="todo_delete")
+	 */	
+	public function deleteAction($id)
+	{
+		// $em = $this->getDoctrine()->getManager();
+		// $todo = $em->getRepository('AppBundle:Todo')->find($id);
+		
+		// $em->remove($todo);
+		// $em->flush();
+		
+		$this->addFlash('notice','Todo Removed');
+		
+		return $this->redirectToRoute('todo_list');
+	}
 }
